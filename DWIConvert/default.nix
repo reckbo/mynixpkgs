3539a2a8462b7e136ebb36d6ebb2847b65f4d114
reckbo <reckbo@bwh.harvard.edu>
2016-07-26 09:56:50 +1000

    init

---
+++ DWIConvert/default.nix
@@ -0,0 +1,25 @@
+{ stdenv, fetchFromGitHub, cmake, ITK, SlicerExecutionModel }:
+
+stdenv.mkDerivation rec {
+  name = "${pname}-${version}";
+  pname = "DWIConvert";
+  version = "4.7.1";
+  src = fetchFromGitHub {
+      owner = "reckbo";
+      repo = pname;
+      rev = "cf9aae8515e5d69b1a027dca3e68c4f70915f541";
+      sha256 = "1rpq4khb2wd23slya582d2cq2bk8hw8b4y7r5iw1sqmizr07wzdc";
+  };
+
+  buildInputs = [
+    cmake
+    ITK
+    SlicerExecutionModel
+  ];
+
+  meta = with stdenv.lib; {
+    description = "Dicom converter";
+    license = licenses.asl20;
+    homepage = https://github.com/BRAINSia/BRAINSTools;
+  };
+}
---
+++ ITK/default.nix
@@ -0,0 +1,29 @@
+{ stdenv, fetchurl, cmake, git, cacert }:
+
+stdenv.mkDerivation rec {
+  name = "${pname}-${version}";
+  pname = "ITK";
+  version = "4.10.0";
+  src = fetchurl {
+      url = "http://ufpr.dl.sourceforge.net/project/itk/itk/4.10/InsightToolkit-4.10.0.tar.xz";
+      sha256 = "0pxijhqsnwcp9jv1d8p11hsj90k8ajpwxhrnn8kk8c56k7y1207a";
+  };
+
+  buildInputs = [
+    cmake
+    git
+  ];
+
+  GIT_SSL_CAINFO = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+
+  cmakeFlags = [
+      "-DModule_ITKDCMTK=ON"
+      "-DModule_ITKIODCMTK=ON"
+  ];
+
+  meta = with stdenv.lib; {
+    description = "Medical imaging processing library";
+    license = licenses.asl20;
+    homepage = http://itk.org;
+  };
+}
