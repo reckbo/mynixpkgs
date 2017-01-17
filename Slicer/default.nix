64dfb47f52a26158aa0da6d5bc98f68debcfbb4d
reckbo <reckbo@bwh.harvard.edu>
2017-01-17 14:01:44 +0000

    Got Slicer working with nix-build

--- Slicer/default.nix
+++ Slicer/default.nix
@@ -1,39 +1,50 @@
-{ stdenv, libSM, libX11, libICE, libXrender, libXext,
-  zlib }:
+{ stdenv, zlib, qt4, mesa, libXt, fontconfig, freetype, glib,
+  libSM, libX11, libICE, libXrender, libXext }:
 
 stdenv.mkDerivation rec {
-  name = "${pname}-${version}";
   pname = "Slicer";
   version = "4.6.2";
-  src = ./Slicer-4.6.2-linux-amd64.tar;
+  name = "${pname}-${version}";
+  src = ./Slicer-4.6.2-linux-amd64.tar.gz;
 
   libPath = stdenv.lib.makeLibraryPath [
+    zlib
     stdenv.cc.cc
+    libXt
+    mesa
+    qt4
     libSM
     libICE
     libXrender
     libXext
-    zlib
     libX11
+    fontconfig
+    freetype
+    glib
   ];
 
   phases = [ "unpackPhase" "installPhase" ];
   installPhase = ''
-    mkdir -p "$out/opt/$pname"
-    cp -r . "$out/opt/$pname"
+    mkdir -p "$out"
+    cp -r . "$out"
     # chmod +x "$out/opt/thomaswasalone/thomasWasAlone"
 
     patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) \
       --set-rpath "$libPath" \
-      "$out/opt/$pname/Slicer"
+      "$out/Slicer"
 
-    mkdir "$out/bin"
-    ln -s "$out/opt/$pname/Slicer" "$out/bin/Slicer"
+    patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) \
+      --set-rpath "$libPath:$out/lib/Slicer-4.6:$out/lib/Python/lib:$out/lib/Teem-1.12.0" \
+      "$out/bin/SlicerApp-real"
+
+    for f in $(find $out -name "*.so"); do
+      patchelf --set-rpath "$libPath" $f
+    done
   '';
 
   meta = with stdenv.lib; {
-    description = "Medical Imaging";
-    license = licenses.lgpl21;
-    homepage = http://teem.sourceforge.net/index.html;
+    description = "Software package for visualization and medical image computing";
+    license = licenses.bsd;
+    homepage = https://www.slicer.org;
   };
 }
--- nixpkgs-custom.nix
+++ nixpkgs-custom.nix
@@ -4,12 +4,12 @@ let
     pkgs = import <nixpkgs> { inherit system; };
     callPackage = pkgs.lib.callPackageWith (pkgs // self);
     self = {
+      Slicer = callPackage ./Slicer { };
       ITK = callPackage ./ITK { };
       SlicerExecutionModel = callPackage ./SlicerExecutionModel { };
       DWIConvert = callPackage ./DWIConvert { };
       teem = callPackage ./teem { };
       ConvertBetweenFileFormats = callPackage ./ConvertBetweenFileFormats { };
-      Slicer = callPackage ./Slicer { };
   };
 in
   pkgs // self
